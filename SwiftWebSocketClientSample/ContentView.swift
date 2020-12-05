f488fb39648e9af0371f4e6e266edeeafa090814
asmz <land.front.high.field@gmail.com>
2020-12-05 22:40:28 +0900

    Initial Commit

---
+++ SwiftWebSocketClientSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftWebSocketClientSample
+//
+//  Created by Akira Shimizu on 2020/12/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftWebSocketClientSample/SwiftWebSocketClientSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWebSocketClientSampleApp.swift
+//  SwiftWebSocketClientSample
+//
+//  Created by Akira Shimizu on 2020/12/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWebSocketClientSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
